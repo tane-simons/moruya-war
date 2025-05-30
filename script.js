23585428611d58fdc7a38e15b75448450dd917fe
tane-simons <163289987+tane-simons@users.noreply.github.com>
2025-05-31 00:57:22 +1000

    add scripting and css for draft event child page

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ window.addEventListener('scroll', () => {
     const homeHeader = document.getElementById('home-header');
     const scrollY = window.scrollY;
     const transparency = scrollY/100;
-
-    homeHeader.style.backgroundColor = `rgba(18, 18, 18, ${transparency})`;
+    if (homeHeader) {
+        homeHeader.style.backgroundColor = `rgba(18, 18, 18, ${transparency})`;
+    }
 });
 
 document.querySelectorAll('.events-card').forEach(card => {
@@ -42,3 +43,47 @@ document.querySelectorAll('#mobile-nav a').forEach(link => {
         hamburger.innerHTML = '&times;';
     });
 });
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const cta = document.getElementById('events-child-cta');
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 820) {
+        if (scrollY > 350) {
+            cta.style.position = 'fixed'
+            cta.style.bottom = '5vw';
+            cta.style.right = '5vw';
+            cta.style.transition = 'all 0.3s ease';
+        } else {
+            cta.style.position = 'relative';
+            cta.style.bottom = '0';
+            cta.style.right = '0';
+            cta.style.transition = 'background-color 0.3s ease, color 0.3s ease';
+        }
+    } else {
+        if (scrollY > 260) {
+            cta.style.position = 'fixed'
+            cta.style.bottom = '-1rem';
+            cta.style.right = '5vw';
+            cta.style.width = '90vw';
+            cta.style.padding = '1rem 0';
+            cta.style.boxShadow = '0 1rem 4rem var(--secondary-color)';
+            cta.style.transition = 'background-color 0.3s ease, color 0.3s ease, right 0.3s ease';
+        } else {
+            cta.style.position = 'relative';
+            cta.style.right = '0';
+            cta.style.padding = '1rem 2rem';
+            cta.style.width = 'fit-content';
+            cta.style.boxShadow = '0 0.2rem 1rem var(--secondary-color)';
+            cta.style.transition = 'background-color 0.3s ease, color 0.3s ease, right 0.3s ease';
+        }
+    }
+});
+
+var map = L.map('map').setView([-35.9107, 150.0778], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+L.marker([-35.9107, 150.0778]).addTo(map)
+    .bindPopup('RSL Memorial Hall')
+    .openPopup();
